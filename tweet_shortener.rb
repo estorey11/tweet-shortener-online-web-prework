aa1a346f10673d59720e9df54f089533ffd2fd2f
Eric Storey <estorey@gmail.com>
2019-03-11 04:28:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,11 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
 
 
