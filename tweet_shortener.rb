c3d238a2f0bd9eb91a80bbfe9fcf2f6c1ce2ece1
Eric Storey <estorey@gmail.com>
2019-03-11 04:14:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require 'pry'
+
+def dictionary
+  words={
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "four" => '4',
+    "for" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet=tweet.split( )
+  tweet.collect {|word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    end
+  }
+  binding.pry
+  tweet.join(" ")
+end
